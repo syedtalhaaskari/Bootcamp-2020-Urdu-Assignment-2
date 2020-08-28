b5947309dc0616724a703786e7cd03fb94aef540
syedtalhaaskari <syedtalhaaskari@gmail.com>
2020-08-28 16:49:13 +0500

    Assignment 2

---
+++ build/precache-manifest.3974d947529ecda1975165382ac557e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f5418ba71b646dff1c87d9140e90e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1c685b419c7de2bc68",
+    "url": "/static/css/main.60f9e1d7.chunk.css"
+  },
+  {
+    "revision": "32a30d4ccbdb9a4ab6de",
+    "url": "/static/js/2.4c695371.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c695371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1c685b419c7de2bc68",
+    "url": "/static/js/main.53bbac8f.chunk.js"
+  },
+  {
+    "revision": "7dd51c7abf6bda0b6e43",
+    "url": "/static/js/runtime-main.2d7e3f53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
